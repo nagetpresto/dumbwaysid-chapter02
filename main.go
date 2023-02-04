211cc9a5c47b4b1a13bae1a8da7b200862140209
unknown <bilqistaz27@gmail.com>
2023-02-05 04:13:37 +0700

    update day08

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
     "html/template"
 	"strconv"
+    "time"
 
     "github.com/gorilla/mux"
 )
@@ -20,28 +21,63 @@ var Data = map[string]interface{}{
 }
 
 type Project struct {
+    Id          int
     Pname       string
     Sdate       string
+    SdateDetail string
     Edate       string
+    EdateDetail string
     Duration    string
     Description string
     Icon1       string
+    Icon1PClass string
+    Icon1P      string
+    Icon1Status string
     Icon2       string
+    Icon2PClass string
+    Icon2P      string
+    Icon2Status string
     Icon3       string
+    Icon3PClass string
+    Icon3P      string
+    Icon3Status string
     Icon4       string
+    Icon4PClass string
+    Icon4P      string
+    Icon4Status string
+}
+
+var ID = []Project{
+    {   
+        Id:             0,
+    },
 }
 
 var ProjectCard = []Project{
     {
         Pname:          "CRUD WEB",
-        Sdate:          "02/02/2023",
-        Edate:          "02/09/2023",
+        Sdate:          "2023-02-02",
+        SdateDetail:    "02 February 2023",
+        Edate:          "2023-09-02",
+        EdateDetail:    "02 September 2023",
         Duration:       "3 Month(s)",
         Description:    "asdas asdsad asdas asd",
-        Icon1:          "fab fa-php me-4",
-        Icon2:          "fab fa-js-square me-4",
-        Icon3:          "fab fa-python me-4",
-        Icon4:          "fab fa-html5 me-4",
+        Icon1:          "fab fa-php me-3",
+        Icon1PClass:    "d-flex align-items-center col-6",
+        Icon1P:         "PHP",
+        Icon1Status:    "checked",
+        Icon2:          "fab fa-js-square me-3",
+        Icon2PClass:    "d-flex align-items-center col-6",
+        Icon2P:         "Javascript",
+        Icon2Status:    "checked",
+        Icon3:          "fab fa-python me-3",
+        Icon3PClass:    "d-flex align-items-center col-6",
+        Icon3P:         "Python",
+        Icon3Status:    "checked",
+        Icon4:          "fab fa-html5 me-3",
+        Icon4PClass:    "d-flex align-items-center col-6",
+        Icon4P:         "HTML",
+        Icon4Status:    "checked",
     },
 }
 
@@ -61,6 +97,7 @@ func main() {
 	route.HandleFunc("/add-project", addProject).Methods("GET")
 	route.HandleFunc("/add-card", addCard).Methods("POST")
     route.HandleFunc("/edit-card/{id}", editCard).Methods("GET")
+    route.HandleFunc("/update-card", updateCard).Methods("POST")
     route.HandleFunc("/delete-card/{id}", deleteCard).Methods("GET")
 	route.HandleFunc("/project/{id}", projectBlog).Methods("GET")
 	route.HandleFunc("/contact-me", contactMe).Methods("GET")
@@ -74,8 +111,6 @@ func main() {
 # Home Routing Function
 --------------------------------------------------------------*/
 func home(w http.ResponseWriter, r *http.Request) {
-    // w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
 	// parsing file
     var tmpl, err = template.ParseFiles("views/index.html")
 	// (in response) if parsing error
@@ -84,10 +119,12 @@ func home(w http.ResponseWriter, r *http.Request) {
         return
     }
 
+    // mapping data to be displayed
     respCard := map[string]interface{}{
         "Data":  Data,
         "ProjectCard": ProjectCard,
     }
+
 	// execute file
     tmpl.Execute(w, respCard)
 }
@@ -96,10 +133,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 # Project Routing Function
 --------------------------------------------------------------*/
 func addProject(w http.ResponseWriter, r *http.Request) {
-    // w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
 	// parsing file
     var tmpl, err = template.ParseFiles("views/project.html")
+
 	// (in response) if parsing error
     if err != nil {
         http.Error(w, err.Error(),http.StatusInternalServerError)
@@ -114,120 +150,370 @@ func addProject(w http.ResponseWriter, r *http.Request) {
 # Add Card Routing Function
 --------------------------------------------------------------*/
 func addCard(w http.ResponseWriter, r *http.Request) {
-    // w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
-	// (in request) only var error >> parsing form html
+	// (in request) output data error >> parsing form html
     err := r.ParseForm()
+
 	// (in response) if parsing error
     if err != nil {
         http.Error(w, err.Error(),http.StatusInternalServerError)
         return
     }
 
-	// getting data
-    pName := r.PostForm.Get("pName")
-    sDate := r.PostForm.Get("sDate")
-    eDate := r.PostForm.Get("eDate")
+	// getting data from input form
+    pName       := r.PostForm.Get("pName")
     description := r.PostForm.Get("description")
-    c1 := r.Form.Get("c1")
-    c2 := r.Form.Get("c2")
-    c3 := r.Form.Get("c3")
-    c4 := r.Form.Get("c4")
+    c1          := r.Form.Get("c1")
+    Icon1PClass := ""
+    Icon1P      := ""
+    Icon1Status := ""
+    c2          := r.Form.Get("c2")
+    Icon2PClass := ""
+    Icon2P      := ""
+    Icon2Status := ""
+    c3          := r.Form.Get("c3")
+    Icon3PClass := ""
+    Icon3P      := ""
+    Icon3Status := ""
+    c4          := r.Form.Get("c4")
+    Icon4PClass := ""
+    Icon4P      := ""
+    Icon4Status := ""
 
+    // parsing date (change format)
+    layoutFormat := "2006-01-02"
+    sDateForm    := r.PostForm.Get("sDate")
+    eDateForm    := r.PostForm.Get("eDate")
+
+    sDate, _ := time.Parse(layoutFormat, sDateForm)
+    sDateDay := strconv.Itoa(sDate.Day())
+    sDateMonth := sDate.Month().String()
+    sDateYear := strconv.Itoa(sDate.Year())
+    sDateFinal := sDateDay + " " + sDateMonth + " " + sDateYear
+
+    eDate, _ := time.Parse(layoutFormat, eDateForm)
+    eDateDay := strconv.Itoa(eDate.Day())
+    eDateMonth := eDate.Month().String()
+    eDateYear := strconv.Itoa(eDate.Year())
+    eDateFinal := eDateDay + " " + eDateMonth + " " + eDateYear
+
+    // conditional for tech icon
     if c1 == "on" {
-        c1 = "fab fa-php me-4"
+        c1          = "fab fa-php me-3"
+        Icon1PClass = "d-flex align-items-center col-6"
+        Icon1P      = "PHP"
+        Icon1Status = "checked"
     }else {
-        c1 = ""
+        c1          = ""
+        Icon1PClass = ""
+        Icon1P      = ""
+        Icon1Status = ""
     }
 
     if c2 == "on" {
-        c2 = "fab fa-js-square me-4"
+        c2          = "fab fa-js-square me-3"
+        Icon2PClass = "d-flex align-items-center col-6"
+        Icon2P      = "Javascript"
+        Icon2Status = "checked"
     }else {
-        c2 = ""
+        c2          = ""
+        Icon2PClass = ""
+        Icon2P      = ""
+        Icon2Status = ""
     }
 
     if c3 == "on" {
-        c3 = "fab fa-python me-4"
+        c3          = "fab fa-python me-3"
+        Icon3PClass = "d-flex align-items-center col-6"
+        Icon3P      = "Python"
+        Icon3Status = "checked"
     }else {
-        c3 = ""
+        c3          = ""
+        Icon3PClass = ""
+        Icon3P      = ""
+        Icon3Status = ""
     }
 
     if c4 == "on" {
-        c4 = "fab fa-html5 me-4"
+        c4          = "fab fa-html5 me-3"
+        Icon4PClass = "d-flex align-items-center col-6"
+        Icon4P      = "HTML"
+        Icon4Status = "checked"
     }else {
-        c4 = ""
+        c4          = ""
+        Icon4PClass = ""
+        Icon4P      = ""
+        Icon4Status = ""
     }
 
+    // create temporary variable for storing data from input form (array)
     var newProjectCard = Project{
         Pname:          pName,
-        Sdate:          sDate,
-        Edate:          eDate,
+        Sdate:          sDateForm,
+        SdateDetail:    sDateFinal,
+        Edate:          eDateForm,
+        EdateDetail:    eDateFinal,
         Duration:       "3 Month(s)",
         Description:    description,
         Icon1:          c1,
+        Icon1PClass:    Icon1PClass,
+        Icon1P:         Icon1P,
+        Icon1Status:    Icon1Status,   
         Icon2:          c2,
+        Icon2PClass:    Icon2PClass,
+        Icon2P:         Icon2P,
+        Icon2Status:    Icon2Status, 
         Icon3:          c3,
+        Icon3PClass:    Icon3PClass,
+        Icon3P:         Icon3P,
+        Icon3Status:    Icon3Status, 
         Icon4:          c4,
+        Icon4PClass:    Icon4PClass,
+        Icon4P:         Icon4P,
+        Icon4Status:    Icon4Status, 
     }
 
+    // adding to the global variable
     ProjectCard = append(ProjectCard, newProjectCard)
-    fmt.Println(ProjectCard)
-    http.Redirect(w, r, "/add-project", http.StatusMovedPermanently)
+
+    // redirect to home
+    http.Redirect(w, r, "/", http.StatusMovedPermanently)
 }
 
 /*--------------------------------------------------------------
 # Edit Card Routing Function
 --------------------------------------------------------------*/
 func editCard(w http.ResponseWriter, r *http.Request){
+    // parsing file
+    var tmpl, err = template.ParseFiles("views/project-update.html")
 
-    // var for storing id
+	// (in response) if parsing error
+    if err != nil {
+        http.Error(w, err.Error(),http.StatusInternalServerError)
+        return
+    }
+
+    // var for storing & converting type of the id
 	id, _ := strconv.Atoi(mux.Vars(r)["id"])
 
+    // update value of id from Data (global var)
+    ID[0].Id = id
+    
+    // defining temporary variable for storing data (array)
     editCard := Project{}
 
+    // looping to find data matches the id
     for i, data := range ProjectCard {
-        if i == id {
+        if i == id {        
             editCard = Project{
                 Pname:          data.Pname,
                 Sdate:          data.Sdate,
+                SdateDetail:    data.SdateDetail,
                 Edate:          data.Edate,
+                EdateDetail:    data.EdateDetail,
                 Duration:       "3 Month(s)",
                 Description:    data.Description,
                 Icon1:          data.Icon1,
+                Icon1PClass:    data.Icon1PClass,
+                Icon1P:         data.Icon1P,
+                Icon1Status:    data.Icon1Status,
                 Icon2:          data.Icon2,
+                Icon2PClass:    data.Icon2PClass,
+                Icon2P:         data.Icon2P,
+                Icon2Status:    data.Icon2Status,
                 Icon3:          data.Icon3,
+                Icon3PClass:    data.Icon3PClass,
+                Icon3P:         data.Icon3P,
+                Icon3Status:    data.Icon3Status,
                 Icon4:          data.Icon4,
+                Icon4PClass:    data.Icon4PClass,
+                Icon4P:         data.Icon4P,
+                Icon4Status:    data.Icon4Status,
             }
         }
-
     }
 
-    // joining id to global var
+	// mapping data to be displayed
     resp := map[string]interface{}{
         "Data":  Data,
-        "EditCard": editCard,
+        "Project": editCard,
+    }
+    
+    // execute file
+    tmpl.Execute(w, resp)
+
+}
+
+/*--------------------------------------------------------------
+# Update Card Routing Function
+--------------------------------------------------------------*/
+func updateCard(w http.ResponseWriter, r *http.Request){
+    // (in request) output data error >> parsing form html
+    err := r.ParseForm()
+
+	// (in response) if parsing error
+    if err != nil {
+        http.Error(w, err.Error(),http.StatusInternalServerError)
+        return
     }
 
-    fmt.Println(resp)
+    // getting data from input form
+    pName       := r.PostForm.Get("pName")
+    description := r.PostForm.Get("description")
+    c1          := r.Form.Get("c1")
+    Icon1PClass := ""
+    Icon1P      := ""
+    Icon1Status := ""
+    c2          := r.Form.Get("c2")
+    Icon2PClass := ""
+    Icon2P      := ""
+    Icon2Status := ""
+    c3          := r.Form.Get("c3")
+    Icon3PClass := ""
+    Icon3P      := ""
+    Icon3Status := ""
+    c4          := r.Form.Get("c4")
+    Icon4PClass := ""
+    Icon4P      := ""
+    Icon4Status := ""
+
+    // parsing date (change format)
+    layoutFormat := "2006-01-02"
+    sDateForm    := r.PostForm.Get("sDate")
+    eDateForm    := r.PostForm.Get("eDate")
+
+    sDate, _ := time.Parse(layoutFormat, sDateForm)
+    sDateDay := strconv.Itoa(sDate.Day())
+    sDateMonth := sDate.Month().String()
+    sDateYear := strconv.Itoa(sDate.Year())
+    sDateFinal := sDateDay + " " + sDateMonth + " " + sDateYear
+
+    eDate, _ := time.Parse(layoutFormat, eDateForm)
+    eDateDay := strconv.Itoa(eDate.Day())
+    eDateMonth := eDate.Month().String()
+    eDateYear := strconv.Itoa(eDate.Year())
+    eDateFinal := eDateDay + " " + eDateMonth + " " + eDateYear
+
+    // conditional for tech icon
+    if c1 == "on" {
+        c1          = "fab fa-php me-3"
+        Icon1PClass = "d-flex align-items-center col-6"
+        Icon1P      = "PHP"
+        Icon1Status = "checked"
+    }else {
+        c1          = ""
+        Icon1PClass = ""
+        Icon1P      = ""
+        Icon1Status = ""
+    }
+
+    if c2 == "on" {
+        c2          = "fab fa-js-square me-3"
+        Icon2PClass = "d-flex align-items-center col-6"
+        Icon2P      = "Javascript"
+        Icon2Status = "checked"
+    }else {
+        c2          = ""
+        Icon2PClass = ""
+        Icon2P      = ""
+        Icon2Status = ""
+    }
+
+    if c3 == "on" {
+        c3          = "fab fa-python me-3"
+        Icon3PClass = "d-flex align-items-center col-6"
+        Icon3P      = "Python"
+        Icon3Status = "checked"
+    }else {
+        c3          = ""
+        Icon3PClass = ""
+        Icon3P      = ""
+        Icon3Status = ""
+    }
+
+    if c4 == "on" {
+        c4          = "fab fa-html5 me-3"
+        Icon4PClass = "d-flex align-items-center col-6"
+        Icon4P      = "HTML"
+        Icon4Status = "checked"
+    }else {
+        c4          = ""
+        Icon4PClass = ""
+        Icon4P      = ""
+        Icon4Status = ""
+    }
+
+    // create temporary variable for storing data from input form (array)
+    var updateProjectCard = Project{
+        Pname:          pName,
+        Sdate:          sDateForm,
+        SdateDetail:    sDateFinal,
+        Edate:          eDateForm,
+        EdateDetail:    eDateFinal,
+        Duration:       "3 Month(s)",
+        Description:    description,
+        Icon1:          c1,
+        Icon1PClass:    Icon1PClass,
+        Icon1P:         Icon1P,
+        Icon1Status:    Icon1Status,   
+        Icon2:          c2,
+        Icon2PClass:    Icon2PClass,
+        Icon2P:         Icon2P,
+        Icon2Status:    Icon2Status, 
+        Icon3:          c3,
+        Icon3PClass:    Icon3PClass,
+        Icon3P:         Icon3P,
+        Icon3Status:    Icon3Status, 
+        Icon4:          c4,
+        Icon4PClass:    Icon4PClass,
+        Icon4P:         Icon4P,
+        Icon4Status:    Icon4Status, 
+    }
+
+    // updating data/value
+    ProjectCard[ID[0].Id].Pname       = updateProjectCard.Pname
+    ProjectCard[ID[0].Id].Sdate       = updateProjectCard.Sdate
+    ProjectCard[ID[0].Id].SdateDetail = updateProjectCard.SdateDetail
+    ProjectCard[ID[0].Id].Edate       = updateProjectCard.Edate
+    ProjectCard[ID[0].Id].EdateDetail = updateProjectCard.EdateDetail
+    ProjectCard[ID[0].Id].Duration    = updateProjectCard.Duration
+    ProjectCard[ID[0].Id].Description = updateProjectCard.Description
+    ProjectCard[ID[0].Id].Icon1       = updateProjectCard.Icon1
+    ProjectCard[ID[0].Id].Icon1PClass = updateProjectCard.Icon1PClass
+    ProjectCard[ID[0].Id].Icon1P      = updateProjectCard.Icon1P
+    ProjectCard[ID[0].Id].Icon1Status = updateProjectCard.Icon1Status   
+    ProjectCard[ID[0].Id].Icon2       = updateProjectCard.Icon2
+    ProjectCard[ID[0].Id].Icon2PClass = updateProjectCard.Icon2PClass
+    ProjectCard[ID[0].Id].Icon2P      = updateProjectCard.Icon2P
+    ProjectCard[ID[0].Id].Icon2Status = updateProjectCard.Icon2Status 
+    ProjectCard[ID[0].Id].Icon3       = updateProjectCard.Icon3
+    ProjectCard[ID[0].Id].Icon3PClass = updateProjectCard.Icon3PClass
+    ProjectCard[ID[0].Id].Icon3P      = updateProjectCard.Icon3P
+    ProjectCard[ID[0].Id].Icon3Status = updateProjectCard.Icon3Status 
+    ProjectCard[ID[0].Id].Icon4       = updateProjectCard.Icon4
+    ProjectCard[ID[0].Id].Icon4PClass = updateProjectCard.Icon4PClass
+    ProjectCard[ID[0].Id].Icon4P      = updateProjectCard.Icon4P
+    ProjectCard[ID[0].Id].Icon4Status = updateProjectCard.Icon4Status
+
+    // redirect to home
+    http.Redirect(w, r, "/", http.StatusMovedPermanently)
 }
 
 /*--------------------------------------------------------------
 # Delete Card Routing Function
 --------------------------------------------------------------*/
 func deleteCard(w http.ResponseWriter, r *http.Request){
+    // var for storing & converting type of the id
     id, _ := strconv.Atoi(mux.Vars(r)["id"])
 
+    // selecting data to be displayed (... for excluding)
     ProjectCard = append(ProjectCard[:id], ProjectCard[id+1:]...)
     http.Redirect(w, r, "/", http.StatusMovedPermanently)
-
 }
 
 /*--------------------------------------------------------------
 # Project Blog Routing Function
 --------------------------------------------------------------*/
 func projectBlog(w http.ResponseWriter, r *http.Request) {
-    // w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
 	// parsing file
 	var tmpl, err = template.ParseFiles("views/project-blog.html")
 
@@ -237,35 +523,52 @@ func projectBlog(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-	// var for storing id
+	// var for storing & converting type of the id
 	id, _ := strconv.Atoi(mux.Vars(r)["id"])
 
+    // update value of id from Data (global var)
+    ID[0].Id = id
+
+    // defining temporary variable for storing data (array)
     projectBlog := Project{}
 
+    // looping to find data matches the id
     for i, data := range ProjectCard {
-        if i == id {
+        if i == id {        
             projectBlog = Project{
                 Pname:          data.Pname,
                 Sdate:          data.Sdate,
+                SdateDetail:    data.SdateDetail,
                 Edate:          data.Edate,
+                EdateDetail:    data.EdateDetail,
                 Duration:       "3 Month(s)",
                 Description:    data.Description,
                 Icon1:          data.Icon1,
+                Icon1PClass:    data.Icon1PClass,
+                Icon1P:         data.Icon1P,
+                Icon1Status:    data.Icon1Status,
                 Icon2:          data.Icon2,
+                Icon2PClass:    data.Icon2PClass,
+                Icon2P:         data.Icon2P,
+                Icon2Status:    data.Icon2Status,
                 Icon3:          data.Icon3,
+                Icon3PClass:    data.Icon3PClass,
+                Icon3P:         data.Icon3P,
+                Icon3Status:    data.Icon3Status,
                 Icon4:          data.Icon4,
+                Icon4PClass:    data.Icon4PClass,
+                Icon4P:         data.Icon4P,
+                Icon4Status:    data.Icon4Status,
             }
         }
-
     }
 
-	// joining id to global var
+	// mapping data to be displayed
     resp := map[string]interface{}{
         "Data":  Data,
         "Project": projectBlog,
     }
     // execute file
-    fmt.Println(resp)
     tmpl.Execute(w, resp)
 
 }
@@ -274,10 +577,9 @@ func projectBlog(w http.ResponseWriter, r *http.Request) {
 # Contact Me Routing Function
 --------------------------------------------------------------*/
 func contactMe(w http.ResponseWriter, r *http.Request) {
-    // w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
 	// parsing file
     var tmpl, err = template.ParseFiles("views/contact-me.html")
+
 	// (in response) if parsing error
     if err != nil {
         http.Error(w, err.Error(),http.StatusInternalServerError)
---
+++ public/js/project.js
@@ -0,0 +1,239 @@
+/*--------------------------------------------------------------
+# Add Project Form
+--------------------------------------------------------------*/
+
+
+//global array to store data
+let project = []
+
+//to get data
+function addProject () {
+
+    //to store data in variable
+    let title = document.getElementById("pName").value
+    let startDate = document.getElementById("sDate").value
+    let endDate = document.getElementById("eDate").value
+    let description = document.getElementById("description").value
+    let c1 = document.getElementById("c1").checked
+    let c2 = document.getElementById("c2").checked
+    let c3 = document.getElementById("c3").checked
+    let c4 = document.getElementById("c4").checked
+    let image = document.getElementById('file').files
+
+    let icon1 = (c1 == true) ? '<i class="fab fa-php"></i>' : '';
+    let icon2 = (c2 == true) ? '<i class="fab fa-js-square"></i>' : '';
+    let icon3 = (c3 == true) ? '<i class="fab fa-python"></i>' : '';
+    let icon4 = (c4 == true) ? '<i class="fab fa-html5"></i>' : '';
+
+    //to animate alert
+    const elem = document.getElementById("alert");   
+    let pos = 20;
+    let id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 53) {
+        clearInterval(id);
+        elem.style.opacity = 1;
+        } else {
+        pos++; 
+        elem.style.opacity = 0.3;
+        elem.style.top = pos + "px";  
+        }
+    }
+
+    //to initialize alert color
+    document.getElementById("alert").style.backgroundColor = "rgb(252, 179, 179)";
+    document.getElementById("alert").style.color = "#965656";
+
+    //to validate data
+    if( title == ""){
+        //to call alert
+        document.getElementById("alert").innerHTML = "Project Name cannot be empty";
+    }
+
+    else if (startDate == ""){
+        //to call alert
+        document.getElementById("alert").innerHTML = "Start Date cannot be empty";
+    }
+
+    else if (endDate == ""){
+        //to call alert
+        document.getElementById("alert").innerHTML = "End Date cannot be empty";
+    }
+
+    else if (description == ""){
+        //to call alert
+        document.getElementById("alert").innerHTML = "Description cannot be empty";
+    }
+
+    else if (icon1 == false && icon2 == false && icon3 == false && icon4 == false){
+        //to call alert
+        document.getElementById("alert").innerHTML = "Technology cannot be empty";
+    }
+
+    else if (image.length == 0){
+        //to call alert
+        document.getElementById("alert").innerHTML = "Image cannot be empty";
+    }
+
+    else {
+        // Convert spesific image to blob object
+        image = URL.createObjectURL(image[0])
+
+        // to store icon
+        let icon = [
+            icon1,
+            icon2,
+            icon3,
+            icon4
+        ]
+        
+        // to get duration
+        let startDate = new Date(document.getElementById("sDate").value)
+        let endDate = new Date(document.getElementById("eDate").value)
+        let diff = endDate.getTime() - startDate.getTime() //in milisec
+        console.log(diff)
+        console.log(startDate)
+        console.log(endDate)
+
+        if (diff < 0) {
+            //to call alert
+            document.getElementById("alert").innerHTML = "Duration is invalid";
+            duration = ""
+        }
+        else {
+            let durYear = Math.floor(diff / (12 * 30 * 24* 3600 * 1000)) //in year
+            let durMonth = Math.floor(diff / (30 * 24* 3600 * 1000)) //in month
+            let durWeek = Math.floor(diff / (7 * 24* 3600 * 1000)) //in week
+            let durDay = Math.floor(diff / (24* 3600 * 1000)) //in day
+
+            if (durYear > 0) {
+                duration = durYear + " Year(s)"
+            }
+            else if (durMonth > 0 ) {
+                duration = durMonth + " Month(s)"
+            }
+            else if (durWeek > 0 ) {
+                duration = durWeek + " Week(s)"
+            }
+            else if (durDay > 0 ) {
+                duration = durDay + " Day(s)"
+            }
+
+            // to filter icon
+            let iconFil = icon.filter(elem => elem[1]).reverse()
+            console.log(iconFil)
+
+            // to store object
+            let dataBlog = {
+                image,
+                title,
+                description,
+                iconFil,
+                duration
+            }
+
+            // to add object to the global array
+            project.push(dataBlog)
+            console.log(project)
+
+            //to call function
+            showProject()
+        }
+
+    }
+}
+
+//to add project
+function showProject () {   
+
+    document.getElementById("project-section").innerHTML = ''
+    for(let i=0; i < project.length; i++) {
+
+        if (project[i].iconFil[0] == undefined) {
+            project[i].iconFil[0] = ""
+        }
+        if (project[i].iconFil[1] == undefined) {
+            project[i].iconFil[1] = ""
+        }
+        if (project[i].iconFil[2] == undefined) {
+            project[i].iconFil[2] = ""
+        }
+        if (project[i].iconFil[3] == undefined) {
+            project[i].iconFil[3] = ""
+        }
+        if (project[i].iconFil[4] == undefined) {
+            project[i].iconFil[4] = ""
+        }
+
+        // for (let a=0; a < 4; a++){
+        //     if (project[i].iconFil[a] == undefined) {
+        //         project[i].iconFil[a] = ""
+        //     }
+        // }
+
+
+        document.getElementById("project-section").innerHTML += `
+            <div class="card-project">
+            <!-- Project Image -->
+            <div class="image-container">
+                <img class="image" src="${project[i].image}" alt="Project Image">
+            </div>
+            <!-- End Project Image -->
+
+            <!-- Project Content -->
+            <div class="card-project-content">
+                <div class="upper-content">
+                    <!-- Project Title -->
+                <div class="p-title" id="title">
+                    <a href="project-blog.html">${project[i].title}</a>
+                </div>
+                <!-- Project Title -->
+
+                <!-- Project Period -->
+                <div class="p-detail" id="period">
+                    Duration: ${project[i].duration}
+                </div>
+                <!-- End Project Period -->
+
+                <!-- Project Description -->
+                <div class="p-desc" id="description">${project[i].description}</div>
+                <!-- Project Description -->
+            </div>
+
+            <div class="bottom-content">
+                <!-- Project Icon -->
+                <span class="p-icon">
+                    <div class="c1" id="c1">${project[i].iconFil[0]}</div>
+                    <div class="c2" id="c2">${project[i].iconFil[1]}</div>
+                    <div class="c3" id="c3">${project[i].iconFil[2]}</div>
+                    <div class="c4" id="c4">${project[i].iconFil[3]}</div>
+                </span>
+                <!-- End Project Icon  -->
+
+                <!-- Project Button -->
+                <div class="p-button">
+                    <button type="button" name="edit" id="edit" class="btn-project">Edit</button>
+                    <button type="button" name="delete" id="delete" class="btn-project">Delete</button>
+                </div>
+                <!-- End Project Button -->
+            </div>
+            
+            </div>
+            <!-- End Project Content -->
+            
+        </div>
+        `
+    }
+
+    //to change alert color
+    document.getElementById("alert").style.backgroundColor = "rgb(185, 245, 170)";
+    document.getElementById("alert").style.color = "rgb(75, 110, 66)";
+
+    //to all alert
+    document.getElementById("alert").innerHTML = "Project has been added"
+
+    //to reset form only without the whole page
+    let form = document.getElementById("form");
+    form.reset();
+
+}
